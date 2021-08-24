089255978d41cca68c997a8509ca29c9d9836698
tripletrouble1 <81735687+tripletrouble1@users.noreply.github.com>
2021-08-24 09:39:53 +0530

    Add files via upload
    first

---
+++ bill.py
@@ -0,0 +1,9 @@
+a=int(input("enter a total amount"))
+b=int(input("tips in percentage"))
+g=b/a
+c=g*100
+amount=a+c
+d=int(input("no of persons want to pay"))
+e=amount/d
+print(f"each person want to pay {e}")
+
