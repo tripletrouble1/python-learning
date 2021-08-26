652e2a7def866454b3f3843d6030fe21ea066b67
tripletrouble1 <81735687+tripletrouble1@users.noreply.github.com>
2021-08-26 11:02:05 +0530

    Add files via upload

---
+++ leapyr.py
@@ -0,0 +1,11 @@
+a=int(input("enter the present year"))
+if (a%4) == 0:
+    if (a%100) == 0:
+        if (a%400) == 0:
+     
+            else:
+                print(f"{a} is not a leap year")
+            else:
+                print(f"{a} is a leap year")
+            else:
+                print(f"{a} is not a leap year")
